4f6347792a7bcb13ae1d499a55452fcf5d650ac2
Pradip Waghmare <waghmarepb@outlook.com>
2020-09-23 23:31:06 +0530

    update in app

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent]
+  // bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('app-element', myElement);
+  }
+  ngDoBootstrap(): void { }
+}
